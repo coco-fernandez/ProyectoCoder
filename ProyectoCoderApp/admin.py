23052f1522076c732693af7a4ff57691cb69edca
coco-fernandez <elo.arg.88@gmail.com>
2022-06-21 02:55:25 -0300

    trabajando con panel admin

--- ProyectoCoderApp/admin.py
+++ ProyectoCoderApp/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 from ProyectoCoderApp.models import *
+from .models import *
 
 # Register your models here.
+
 class CursoAdmin(admin.ModelAdmin):
 
     list_display = ('nombre', 'comision')
@@ -17,16 +19,18 @@ class EstudianteAdmin(admin.ModelAdmin):
 class ProfesorAdmin(admin.ModelAdmin):
 
     list_display = ('nombre', 'apellido', 'profesion')
-
+    readonly_fields=("profesion",)
 
 class EntregableAdmin(admin.ModelAdmin):
 
     list_display = ('nombre', 'fechaEntrega', 'entregado')
+    
 
 
-admin.site.register(Curso, CursoAdmin)
-admin.site.register(Estudiante, EstudianteAdmin)
-admin.site.register(Profesor, ProfesorAdmin)
-admin.site.register(Entregable, EntregableAdmin)
+
+admin.site.register(Curso,CursoAdmin)
+admin.site.register(Estudiante,EstudianteAdmin)
+admin.site.register(Profesor,ProfesorAdmin)
+admin.site.register(Entregable,EntregableAdmin)
 
 # admin, admin -> python manage.py createsuperuser
