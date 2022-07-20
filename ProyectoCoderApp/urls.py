9987f7c8fbaa6d86864bc77a1c53a0b8851a136e
coco-fernandez <elo.arg.88@gmail.com>
2022-07-19 23:43:24 -0300

    Todo funcionando hasta clase 23

--- ProyectoCoderApp/urls.py
+++ ProyectoCoderApp/urls.py
@@ -8,9 +8,12 @@ from .views import *
 urlpatterns = [
     path('',index,name="index"),
     path('productores/',productores,name="productores"),
+    path('crear_productor/',crear_productor,name="crear_productor"),
     path('eliminar_productor/<productor_id>', eliminar_productor, name="eliminar_productor"),
     path('editar_productor/<productor_id>', editar_productor, name="editar_productor"),
+    
     path('bandas/', bandas, name="bandas"),
+    path('crear_banda/',crear_banda,name="crear_banda"),
     path('eliminar_banda/<banda_id>', eliminar_banda, name="eliminar_banda"),
     path('editar_banda/<banda_id>', editar_banda, name="editar_banda"),
     
@@ -18,8 +21,7 @@ urlpatterns = [
     path('eliminar_estudio/<estudio_id>', eliminar_estudio, name="eliminar_estudio"),
     path('editar_estudio/<estudio_id>', editar_estudio, name="editar_estudio"),
     path('crear_estudio/',crear_estudio,name="crear_estudio"),
-    path('crear_banda/',crear_banda,name="crear_banda"),
-    path('crear_productor/',crear_productor,name="crear_productor"),
-    # path('buscar_comision/',buscar_comision,name="buscar_comision"),
-    # path('base',base),
+    
+    
+    
 ]
--- ProyectoCoderApp/views.py
+++ ProyectoCoderApp/views.py
@@ -7,6 +7,9 @@ from httplib2 import Http
 from .forms import nuevo_banda, nuevo_estudio, nuevo_productor
 from ProyectoCoderApp.models import Bandas, Estudios, Productores
 from django.db.models import Q
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 
 
@@ -14,6 +17,11 @@ from django.db.models import Q
 
 def index(request):
     return render(request,"ProyectoCoderApp/index.html",)
+
+def base (request):
+    return render(request,'ProyectoCoderApp/base.html',{})
+
+
 
 def estudios(request):
     if request.method == "POST":
@@ -27,6 +35,30 @@ def estudios(request):
     estudios = Estudios.objects.all()
 
     return render(request,"ProyectoCoderApp/estudios.html",{"estudios":estudios})
+
+def crear_estudio(request):    # clase de creacion de curso por formulario de web
+
+    if request.method=="POST":    #post
+        
+        formulario=nuevo_estudio(request.POST)
+        
+        if formulario.is_valid():
+            
+            info_estudio=formulario.cleaned_data
+        
+            estudio=Estudios(nombre=info_estudio["nombre"],ubicacion=info_estudio["ubicacion"],cantidad_salas=info_estudio["cantidad_salas"])
+            
+            estudio.save()    #guarda en la DB
+            
+            return redirect("estudios")
+        
+        else:
+            return render(request,'ProyectoCoderApp/formulario_estudio.html',{"form":formulario})
+    
+    else:
+        formulario=nuevo_estudio()
+        
+        return render(request,'ProyectoCoderApp/formulario_estudio.html',{"form":formulario})
 
 def eliminar_estudio(request,estudio_id):
 
@@ -59,6 +91,8 @@ def editar_estudio(request,estudio_id):
     
     return render(request,"ProyectoCoderApp/formulario_estudio.html",{"form":formulario})
 
+
+
 def bandas(request):
     if request.method == "POST":
 
@@ -71,6 +105,30 @@ def bandas(request):
     bandas = Bandas.objects.all()
 
     return render(request,"ProyectoCoderApp/bandas.html",{"bandas":bandas})
+
+def crear_banda(request):    # clase de creacion de curso por formulario de web
+
+    if request.method=="POST":    #post
+        
+        formulario=nuevo_banda(request.POST)
+        
+        if formulario.is_valid():
+            
+            info_banda=formulario.cleaned_data
+        
+            banda=Bandas(nombre=info_banda["nombre"],genero=info_banda["genero"],cantidad_integrantes=info_banda["cantidad_integrantes"])
+            
+            banda.save()    #guarda en la DB
+            
+            return redirect("bandas")
+        
+        else:
+            return render(request,'ProyectoCoderApp/formulario_banda.html',{"form":formulario})
+    
+    else:
+        formulario=nuevo_banda()
+        
+        return render(request,'ProyectoCoderApp/formulario_banda.html',{"form":formulario})
 
 def eliminar_banda(request,banda_id):
 
@@ -103,6 +161,8 @@ def editar_banda(request,banda_id):
     
     return render(request,"ProyectoCoderApp/formulario_banda.html",{"form":formulario})
 
+
+
 def productores(request):
     if request.method == "POST":
 
@@ -115,6 +175,30 @@ def productores(request):
     productores = Productores.objects.all()
 
     return render(request,"ProyectoCoderApp/productores.html",{"productores":productores})
+
+def crear_productor(request):    # clase de creacion de curso por formulario de web
+
+    if request.method=="POST":    #post
+        
+        formulario=nuevo_productor(request.POST)
+        
+        if formulario.is_valid():
+            
+            info_productor=formulario.cleaned_data
+        
+            productor=Productores(nombre=info_productor["nombre"],apellido=info_productor["apellido"],email=info_productor["email"])
+            
+            productor.save()    #guarda en la DB
+            
+            return redirect("productores")
+        
+        else:
+            return render(request,'ProyectoCoderApp/formulario_productor.html',{"form":formulario})
+    
+    else:
+        formulario=nuevo_productor()
+        
+        return render(request,'ProyectoCoderApp/formulario_productor.html',{"form":formulario})
 
 def eliminar_productor(request,productor_id):
 
@@ -147,83 +231,5 @@ def editar_productor(request,productor_id):
     
     return render(request,"ProyectoCoderApp/formulario_productor.html",{"form":formulario})
 
-def crear_estudio(request):    # clase de creacion de curso por formulario de web
 
-    if request.method=="POST":    #post
-        
-        formulario=nuevo_estudio(request.POST)
-        
-        if formulario.is_valid():
-            
-            info_estudio=formulario.cleaned_data
-        
-            estudio=Estudios(nombre=info_estudio["nombre"],ubicacion=info_estudio["ubicacion"],cantidad_salas=info_estudio["cantidad_salas"])
-            
-            estudio.save()    #guarda en la DB
-            
-            return redirect("estudios")
-        
-        else:
-            return render(request,'ProyectoCoderApp/formulario_estudio.html',{"form":formulario})
-    
-    else:
-        formulario=nuevo_estudio()
-        
-        return render(request,'ProyectoCoderApp/formulario_estudio.html',{"form":formulario})
-
-
-def crear_productor(request):    # clase de creacion de curso por formulario de web
-
-    if request.method=="POST":    #post
-        
-        formulario=nuevo_productor(request.POST)
-        
-        if formulario.is_valid():
-            
-            info_productor=formulario.cleaned_data
-        
-            productor=Productores(nombre=info_productor["nombre"],apellido=info_productor["apellido"],email=info_productor["email"])
-            
-            productor.save()    #guarda en la DB
-            
-            return redirect("productores")
-        
-        else:
-            return render(request,'ProyectoCoderApp/formulario_productor.html',{"form":formulario})
-    
-    else:
-        formulario=nuevo_productor()
-        
-        return render(request,'ProyectoCoderApp/formulario_productor.html',{"form":formulario})
-
-
-def crear_banda(request):    # clase de creacion de curso por formulario de web
-
-    if request.method=="POST":    #post
-        
-        formulario=nuevo_banda(request.POST)
-        
-        if formulario.is_valid():
-            
-            info_banda=formulario.cleaned_data
-        
-            banda=Bandas(nombre=info_banda["nombre"],genero=info_banda["genero"],cantidad_integrantes=info_banda["cantidad_integrantes"])
-            
-            banda.save()    #guarda en la DB
-            
-            return redirect("bandas")
-        
-        else:
-            return render(request,'ProyectoCoderApp/formulario_banda.html',{"form":formulario})
-    
-    else:
-        formulario=nuevo_banda()
-        
-        return render(request,'ProyectoCoderApp/formulario_banda.html',{"form":formulario})
-
-
-
-
-def base (request):
-    return render(request,'ProyectoCoderApp/base.html',{})
 
